d471e87f5fd211ebd194f9b57013c1370ca334af
Ducpm <minhducpham.it@gmail.com>
2018-12-24 16:59:26 +0700

    Duc

--- development/js/main.js
+++ development/js/main.js
@@ -1,35 +1,23 @@
 (function($){
 	"use strict";
-	
-	$('.block-header .menubar').on('click', function() {
-		$(this).toggleClass('menu_open');
-		$(this).parents('body').toggleClass('menu_open');
-	});
+    // Slide Carousel
+    $(document).ready(function () {
+        $(".owl-carousel").each(function (index, el) {
+            var config = $(this).data();
+            config.navText = ['<i class="icon icon-arrow-left-circle"></i>', '<i class="icon icon-arrow-right-circle"></i>'];
+            config.smartSpeed = "800";
 
-	// Zoom Web on in all browser
-    function Zoom() {
-        var winHeight = $(window).height();
-        var zoom = 1;
-        var bodyMaxHeight = 780;
-        zoom = winHeight/bodyMaxHeight;
-        /* Firefox */
-        var winWidth = $(window).width();
-        var widthFirefox = winWidth/zoom;
-        if(navigator.userAgent.indexOf("Firefox") != -1) {
-            $('#Zoom').css({
-                '-moz-transform': 'scale('+zoom+')',  /* Firefox */
-                'transform-origin': '0 0',
-                'width': widthFirefox,
-            });
-        } else {
-            $('#Zoom').css({
-                'zoom': zoom,
-            });
-        }
-    }
-    Zoom();
-    $(window).on('load resize', function() {
-        Zoom();
+            if ($(this).hasClass('owl-style2')) {
+                config.animateOut = "fadeOut";
+                config.animateIn = "fadeIn";
+            }
+
+            if ($(this).hasClass('dotsData')) {
+                config.dotsData = "true";
+            }
+
+            $(this).owlCarousel(config);
+        });
     });
 
 })(jQuery); // End of use strict
