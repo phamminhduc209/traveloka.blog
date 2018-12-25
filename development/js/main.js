97f90deaba474e5102c8061931caf1b7f55f090b
Ducpm <minhducpham.it@gmail.com>
2018-12-25 16:33:28 +0700

    Duc

--- development/js/main.js
+++ development/js/main.js
@@ -20,4 +20,21 @@
         });
     });
 
+    /*  [ Sticky Menu ] */
+    $('.bl-header').sticky({ topSpacing: 0 });
+
+    // ===== Scroll to Top ==== 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 200) {
+            $('#return-to-top').addClass('td-scroll-up-visible');
+        } else {
+            $('#return-to-top').removeClass('td-scroll-up-visible');
+        }
+    });
+    $('#return-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 'slow');
+    });
+
 })(jQuery); // End of use strict
